cfede65f2a44fa0c2057a2b135ae838fb0f4e2a9
danaerodriguez96 <danaerodriguez96@gmail.com>
2019-06-27 21:18:21 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,37 +1,37 @@
 def addition(num1, num2)
 num1 + num2
-puts addition(4, 5)
 end
-addition(4, 5)
+puts addition(4, 5)
+
 
 
 
 def subtraction(num1, num2)
 num1 - num2
-puts subtraction(10, 5)
 end
-subtraction(10, 5)
+puts subtraction(10, 5)
+
 
 
 
 def division(num1, num2)
 num1 / num2
-puts division(50, 2)
 end
-division(50, 2)
+puts division(50, 2)
+
 
 def multiplication(num1, num2)
 num1 * num2
-puts multiplication(4, 30)
 end
-multiplication(4, 30)
+puts multiplication(4, 30)
+
 
 
 def modulo(num1, num2)
 num1 % num2
-puts modulo(34, 5)
 end
-modulo(34, 5)
+puts modulo(34, 5)
+
 
 def square_root(num)
 end
